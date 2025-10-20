5287697451dfb1929a6d9934036fd130ef90ca88
Brainlessnoobcoder <dnyaneshwarw000@outlook.com>
2025-10-20 13:15:22 +0530

    added a Global Function File to keep things organized and set up the interval for shop item creation(UNTESTED)

--- js/shop.js
+++ js/shop.js
@@ -5,7 +5,9 @@ window.addEventListener('load', function(){
     var item2price = 500;
 
     /* if section criteria kept intentionally low for testing change it later after done like 450 to 500*/
-    if(actualScore >= 5 && !document.getElementById('item2')){
+
+    var interval_var = setInterval(function(){
+        if(actualScore >= 5 && !document.getElementById('item2')){
             var item2Card = document.createElement('div')
             item2Card.id = 'item2';
             item2Card.innerHTML = `
@@ -13,5 +15,9 @@ window.addEventListener('load', function(){
             <p>Are you getting bored by clicking are Your fingers aching then you just may need this</p>
             <button id="item2Btn">Buy (${item2price} points)</button>`;
             shopSec.appendChild(item2Card);
+            clearInterval(interval_var);
         }
+    }, 500);
+
+    
 });
